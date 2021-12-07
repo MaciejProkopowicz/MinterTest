c141251ed7a665ad43a155a7d798769e3647d36e
Maciej Prokopowicz <promacio007@gmail.com>
2021-12-07 22:04:48 +0100

    MY First Test Deployment of DAPP

---
+++ precache-manifest.83c8109ea32fc1e5737e650bfc1e46c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95b5b5ad0c87722844d3ccc7f8806e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f39739ebc2c1ed702b3",
+    "url": "/static/css/main.2c6c3ec3.chunk.css"
+  },
+  {
+    "revision": "52e0f08154b853ca6e12",
+    "url": "/static/js/2.76a2d2bf.chunk.js"
+  },
+  {
+    "revision": "ca2f69eb275a12548318820e5953aad7",
+    "url": "/static/js/2.76a2d2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128c19e152688d017d8a",
+    "url": "/static/js/3.6c9b13de.chunk.js"
+  },
+  {
+    "revision": "3f39739ebc2c1ed702b3",
+    "url": "/static/js/main.5a30084b.chunk.js"
+  },
+  {
+    "revision": "2d3dc08e5e90f1088ab2",
+    "url": "/static/js/runtime-main.5490b315.js"
+  }
+]);
